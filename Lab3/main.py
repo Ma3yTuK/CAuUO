1ba2f38e9b4d867de01256190ea6289033fb7503
Nazbl4 <nazarmatveev2003@gmail.com>
2024-10-28 23:03:12 +0300

    Tell me how bad this commit is

--- Lab3/main.py
+++ Lab3/main.py
@@ -4,9 +4,9 @@ from numpy.linalg import inv
 
 
 ###values###
-v = np.array([  [   1,  2,  3   ]   ])
-c = np.array([  [   1,  2,  3   ]   ])
-B = 3
+v = np.array([   3,  4,  5,  8,  9   ])
+c = np.array([   1,  6,  4,  7,  6   ])
+B = 13
 ORT = np.zeros((v.size, B))
 x = np.zeros(ORT.shape)
 ###values###
@@ -15,8 +15,8 @@ x = np.zeros(ORT.shape)
 def forward_method():
     for k in range(ORT.shape[0]):
         for b in range(ORT.shape[1]):
-            if K == 0:
-                if v[k] <= b:
+            if k == 0:
+                if v[k] <= b + 1:
                     ORT[k, b] = c[k]
                     x[k, b] = 1
                 else:
@@ -24,8 +24,10 @@ def forward_method():
                     x[k, b] = 0
             else:
                 tmp = ORT[k - 1, b]
-                if v[k] <= b:
-                    tmp1 = ORT[k - 1, b - b[k]] + c[k]
+                if v[k] <= b + 1:
+                    tmp1 = c[k]
+                    if b >= v[k]:
+                        tmp1 += ORT[k - 1, b - v[k]]
                     if tmp1 > tmp:
                         ORT[k, b] = tmp1
                         x[k, b] = 1
@@ -39,12 +41,12 @@ def forward_method():
 def backward_method():
     result = np.zeros(v.size)
 
-    b = ORT[-1, -1]
+    b = B - 1
     for k in reversed(range(v.size)):
         result[k] = x[k, b]
-        b -= result[k] * v[k]
+        b -= round(result[k] * v[k])
 
-    return result
+    return result, B - b - 1
 
 
 def main():
